aba922c96a6cad2f109ee3e83b5a535a79becd4d
Shridhar <36041359+shridharMe@users.noreply.github.com>
2021-12-07 16:10:00 +0000

    Update standardPipeline.groovy

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -2,39 +2,39 @@
 def call(Map pipelineParams) {
  
        stage ('print Params') {
-            steps {
+    
                   sh '''
                       echo "terraform init"
                     '''
  
-                }
+               
         }
         stage ('terraform init') {
-            steps {
+            
                   sh '''
                       echo "terraform init"
                     '''
  
-                }
+               
         }
         stage ('terraform plan') {
-            steps {
+          
                   sh '''
                       echo "terraform plan"
                     '''
  
-                }
+             
         }
         stage ('terraform apply') {
            when {
                 expression { env.GIT_BRANCH == 'origin/master' }   
             }
-            steps {
+           
                   sh '''
                       echo "terraform apply"
                     '''
  
-                }
+             
         }
  
     }
