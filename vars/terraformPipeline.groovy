a14ef43b04ca08f7e804edc72aad370bf257a69e
Shridhar <36041359+shridharMe@users.noreply.github.com>
2021-12-14 09:56:00 +0000

    Update terraformPipeline.groovy

--- vars/terraformPipeline.groovy
+++ vars/terraformPipeline.groovy
@@ -1,5 +1,75 @@
 #!/usr/bin/env groovy
+def call() {
+ pipeline {
+    agent  any
+    options {
+        buildDiscarder(logRotator(numToKeepStr: '5', daysToKeepStr: '14'))
+        timestamps()
+    }
+    parameters { 
+        choice(choices: 'dev', description: 'Select the env to deploy', name: 'ENV_NAME')
+        choice(choices: 'devops', description: 'Select the team to deploy', name: 'SQUAD_NAME')
+    }
+    stages{
+    
+       stage ('print Params') {
+    
+                  println params
+               
+        }
+        stage ('terraform init') {
+            
+                  sh '''
+                      echo "terraform init"
+                    '''
+ 
+               
+        }
+        stage ('terraform plan') {
+          
+                  sh '''
+                      echo "terraform plan"
+                    '''
+ 
+             
+        }
+        stage ('terraform apply') {
+         if (env.GIT_BRANCH == 'origin/master' ){
+          
+                sh '''
+                 echo "terraform apply"
+         '''
+         }
+    
 
-def call(String name = 'human') {
-    echo "Hello, ${name}, you just called jenkins library function"
+        }
+        
+    }
+    post { 
+        always {
+            script{
+                   echo " build cleanup "
+            }
+        }
+        success { 
+              script {
+                      sh '''
+                       echo " build successfull "
+                      '''
+                }
+        }
+        failure {
+            script {
+                    
+                      sh '''
+                       echo " build failed "
+                      '''
+             }
+        }
+    }
+    
+}
+ 
+    
+ 
 }
