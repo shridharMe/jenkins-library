f5ae3fb4df97a5277fe7522d3e9647b0f58d41ca
Shridhar <36041359+shridharMe@users.noreply.github.com>
2021-12-14 12:10:57 +0000

    Update terraformPipeline.groovy

--- vars/terraformPipeline.groovy
+++ vars/terraformPipeline.groovy
@@ -41,7 +41,8 @@ def call() {
         stage ('terraform test') {
             steps {
                 script{
-                    cucumber buildStatus: 'FAILURE', customCssFiles: '', customJsFiles: '', failedFeaturesNumber: 1, failedScenariosNumber: 1, failedStepsNumber: 1, fileIncludePattern: '**/*.json', jsonReportDirectory: 'testing/result/', pendingStepsNumber: 1, reportTitle: 'terraform-complaince', skippedStepsNumber: 1, sortingMethod: 'ALPHABETICAL', stopBuildOnFailedReport: true, undefinedStepsNumber: 1
+                    sh "ls -lrt testing/result/"
+                    cucumber buildStatus: 'FAILURE', customCssFiles: '', customJsFiles: '', failedFeaturesNumber: 1, failedScenariosNumber: 1, failedStepsNumber: 1, fileIncludePattern: '*.json', jsonReportDirectory: 'testing/result/', pendingStepsNumber: 1, reportTitle: 'terraform-complaince', skippedStepsNumber: 1, sortingMethod: 'ALPHABETICAL', stopBuildOnFailedReport: true, undefinedStepsNumber: 1
                 }
             }
         }
